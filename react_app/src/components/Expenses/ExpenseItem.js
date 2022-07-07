1abcb65f3e6842083a03513f5bdf5e188081e1ed
Alex <opetrovskyi423@gmail.com>
2022-07-07 19:04:17 +0200

    props and useState

--- react_app/src/components/Expenses/ExpenseItem.js
+++ react_app/src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 const ExpenseItem = ({date, title, price}) => {
 
+const [firstTitle, setFirstTitle] = useState(title);
+
+    const clickHandler = ()=> {
+        setFirstTitle('UPDATE')
+        console.log('fefe')
+    };
 
     return (
         <Card className='expense-item'>
 
             <ExpenseDate date={date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{firstTitle}</h2>
                 <div className='expense-item__price'>{price}$</div>
             </div>
+            <button onClick={clickHandler}>change</button>
         </Card>
-
     );
 };
 
--- react_app/src/components/Expenses/Expenses.js
+++ react_app/src/components/Expenses/Expenses.js
@@ -3,7 +3,6 @@ import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 
 const Expenses = ({expenses}) => {
-
     return (
         <div className='expenses'>
             <ExpenseItem title={expenses[0].title} date={expenses[0].date} price={expenses[0].amount}/>
